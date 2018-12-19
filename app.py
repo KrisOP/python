e69985b6a0747e57ca79e9d120dab8c5f52e0160
KrisOP <kortega070@gmail.com>
2018-12-19 16:58:55 -0600

    Inicio de practicas

---
+++ app.py
@@ -0,0 +1,16 @@
+
+def mensajeEnFuncion():#definiendo funciones
+
+    print ("Hello wordl!")
+    print (5+6)
+
+    if 5+1 < 10:
+        print ("6 es menor que 10")
+    else:
+        print ("esta pasado")
+
+mensajeEnFuncion()#llamada a la funcion
+
+mensajeEnFuncion()
+
+mensajeEnFuncion()
