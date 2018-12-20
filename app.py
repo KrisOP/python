a780728bcf7b3107e7e2f450558309305236e7e6
KrisOP <kortega070@gmail.com>
2018-12-19 19:02:55 -0600

    funciones con parametros

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 def mensajeEnFuncion():#definiendo funciones
 
     print ("Hello wordl!")
@@ -14,3 +13,19 @@ mensajeEnFuncion()#llamada a la funcion
 mensajeEnFuncion()
 
 mensajeEnFuncion()
+
+
+#Funciones con PARAMETROS
+
+"""Este es otro comentario"""
+
+def suma(num1,num2):
+    print("la suma de num1 y num2 es ")
+    return num1+num2
+
+resultado=suma(5,2)
+print (resultado)
+
+resultado=suma(4,6)
+
+print (resultado)
