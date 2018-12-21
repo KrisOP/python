ec425bb1c38fc0f459381a4a3d7125baacec6dd7
KrisOP <kortega070@gmail.com>
2018-12-21 11:04:33 -0600

    definiendo tuplas y diccionarios dentro de otro diccionario

--- diccionario.py
+++ diccionario.py
@@ -14,7 +14,7 @@ del miDiccionario["Francia"] #eliminar elemento del diccionario con la clave
 
 print (miDiccionario)"""
 
-miDiccionario ={"Alemania":"Berlin", 22:"Kris","ortega":12}
+"""miDiccionario ={"Alemania":"Berlin", 22:"Kris","ortega":12}
 print (miDiccionario)
 print (miDiccionario[22])
 
@@ -23,4 +23,11 @@ miTupla=["Espania","Argentina","Costa Rica"]
 miDiccionario={miTupla[0]:"Madrid",miTupla[1]:"Buenos Aires",miTupla[2]:"San Jose"}#combinando tuplas con diccionarios
 
 #print (miDiccionario[miTupla[1]])
-print (miDiccionario["Argentina"])
+print (miDiccionario["Argentina"])"""
+
+#miDiccionario={23:"Jordan", "nombre":"Jordan","equipo":"chicago", "anillos":[1991,1992,1993,1996,1998]}#definiendo tupla dentro del diccionario
+miDiccionario={23:"Jordan", "nombre":"Jordan","equipo":"chicago", "anillos":{"temporadas":[1991,1992,1993,1996,1998]}}#definiendo diccionario y tuplas dentro del diccionario
+#print (miDiccionario)
+
+print (miDiccionario["anillos"])
+
