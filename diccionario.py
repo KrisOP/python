e9fccfae93b19d39569c7f7a639bcd664d909fb4
KrisOP <kortega070@gmail.com>
2018-12-21 10:47:35 -0600

    agregar, eliminar, modificar e imprimir elementos de diccionarios

---
+++ diccionario.py
@@ -0,0 +1,15 @@
+miDiccionario={"Alemania":"Berlin", "Francia":"paris","Nicaragua":"Managua"}
+
+print(miDiccionario["Nicaragua"])#imprimir datos del diccionario usando las claves
+
+miDiccionario["Italia"]="Lisboa"#agregar un elemento con al diccionario
+
+print (miDiccionario)
+
+miDiccionario["Italia"]="Roma"#cambiar un nuevo valor a la clave "Italia"
+
+print (miDiccionario)
+
+del miDiccionario["Francia"] #eliminar elemento del diccionario con la clave
+
+print (miDiccionario)
