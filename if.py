eead48846d3fbfeef64e3713ea14da7909f1ab16
KrisOP <kortega070@gmail.com>
2018-12-21 11:31:41 -0600

    uso de condicional if

--- if.py
+++ if.py
@@ -1,3 +1,7 @@
+print ("programa de evaluacion de notas")
+#todo valor capturado del teclado es leido como string
+nota_alumno=input("Introduzca la nota del alumno")#detener el programa hasta recibir un valor del teclado
+
 def evaluacion(nota):
     valoracion="aprobado"
     if nota<5:
@@ -6,5 +10,5 @@ def evaluacion(nota):
     else:
         print (valoracion)
 
-evaluacion(4)
+evaluacion(int(nota_alumno))
             
