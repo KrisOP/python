890253eb2317f44b54dda801c0cb41332099b386
KrisOP <kortega070@gmail.com>
2018-12-21 10:56:06 -0600

    combinando tuplas con diccionarios

--- diccionario.py
+++ diccionario.py
@@ -17,3 +17,10 @@ print (miDiccionario)"""
 miDiccionario ={"Alemania":"Berlin", 22:"Kris","ortega":12}
 print (miDiccionario)
 print (miDiccionario[22])
+
+miTupla=["Espania","Argentina","Costa Rica"]
+
+miDiccionario={miTupla[0]:"Madrid",miTupla[1]:"Buenos Aires",miTupla[2]:"San Jose"}#combinando tuplas con diccionarios
+
+#print (miDiccionario[miTupla[1]])
+print (miDiccionario["Argentina"])
