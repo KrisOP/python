9dbd68baca2003317178221ecf8caff912481fe9
KrisOP <kortega070@gmail.com>
2018-12-27 11:28:54 -0600

    uso de range

--- for.py
+++ for.py
@@ -2,12 +2,18 @@ for i in ["Primavera","verano","otonio","invierno"]:
     print ("Hola ", end=" ")#no haga un salto de linea en la siguiente interaccion
 
 email=False
+contador=0
 miEmail=input("ingrese su correo  ")
 for i in miEmail: #recorre caracter a caracter 
-    if (i=="@"):
-        email=True    
+    if (i=="@" or i=="."):
+        contador=contador+1   
 
-if email:#es equivalente a email==true
+"""if email:#es equivalente a email==true
     print ("Es un correo valido")
 else:
-    print("EL correo no es valido")
+    print("EL correo no es valido")"""
+
+if contador==2:
+    print ("email es correcto")
+else:
+    print ("Email incorrecto")
