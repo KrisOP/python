180345a476591312be4f1c857f4bb7acc463ee61
KrisOP <kortega070@gmail.com>
2018-12-27 11:17:15 -0600

    recorrer caracter a caracter un arreglo con un for

--- for.py
+++ for.py
@@ -1,2 +1,6 @@
 for i in ["Primavera","verano","otonio","invierno"]:
     print ("Hola ", end=" ")#no haga un salto de linea en la siguiente interaccion
+
+
+for i in "krisortega15@hotmail.es":#recorre caracter a caracter 
+    print ("caracteres")    
