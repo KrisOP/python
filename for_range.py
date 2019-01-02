da8082d3e51ebbbecbe96c6086905c958b77626f
KrisOP <kortega070@gmail.com>
2019-01-02 09:28:16 -0600

    uso for con range para conteo numerico con parametros

--- for_range.py
+++ for_range.py
@@ -1,2 +1,3 @@
-for i in range(5):
-    print (f"Valor de la variable {i}")
+for i in range(5,50,3):#recorre desde 5 hasta el 49 e incrementa de 3 en 3
+    print (f"Valor de la variable {i}")#funcion f para concatenar el valor de la variable del for en una cadena
+
