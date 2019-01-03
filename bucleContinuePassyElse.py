97a7b935727bf66cc5ef9ab549e50b4e08acf73a
KrisOP <kortega070@gmail.com>
2019-01-03 10:59:49 -0600

    uso de continue

--- bucleContinuePassyElse.py
+++ bucleContinuePassyElse.py
@@ -2,3 +2,15 @@ for letra in "python":
     if letra=="h":
         continue
     print ("Viendo letra: "+letra)
+
+nombre="kristopher ortega"
+
+contador=0
+
+for i in nombre:
+    if i==" ":
+        continue#si existe un espacio ignorar las siguientes instrucciones continuando con la sigiente itracion
+
+    contador+=1
+
+print(contador)
