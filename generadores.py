4538dd6649265d71a649ed9ee7ee4867113c52fc
KrisOP <kortega070@gmail.com>
2019-01-03 17:23:37 -0600

    uso de funcion next en generadores

--- generadores.py
+++ generadores.py
@@ -1,4 +1,4 @@
-def generaPares(limite):
+"""def generaPares(limite):
     num=1
     miLista=[]
     while num<limite:
@@ -7,6 +7,29 @@ def generaPares(limite):
 
     return miLista
 
+print(generaPares(10))"""
+
+#queremos imprimir los pirmeros 3 elementos
+#USO DE GENERADORES
+def generaPares(limite):
+    num=1
+    while num<limite:
+        yield num*2#yield construye un objeto iterable
+        num=num+1
+devuelPares=generaPares(10)
+
+#for i in devuelPares:
+ #   print (i)
+
+print (next(devuelPares))#  que devuelve solalamente el primer valor almacenado en el interior del objeto generador
+
+#ENTRE LLAMADA Y LLAMADA EL OBJETO GENERADOR ENTRA EN ESTADO DE SUSPENCION, A LA SIGUIENTE LLAMADA, CONTINUA CON LA LISTA DE NUMEROS
+print("Aqui podria ir mas codigo...")
+
+print (next(devuelPares))
+
+print("Aqui podria ir mas codigo...")
+
+print (next(devuelPares))
 
 
-print(generaPares(10))
