2e0560bd39a4a7787db1032767796bff3e37d697
KrisOP <kortega070@gmail.com>
2019-01-04 16:43:05 -0600

    lanzando excepciones usando (raise)

--- excepcionesIII.py
+++ excepcionesIII.py
@@ -1,4 +1,9 @@
 def evalueEdad(edad):
+
+    if edad<0:
+        #especificar el tipo de excepcion que lanzara el programa cuando se cumppla la condicion edad<0
+        #estas excepciones no se capturan, lo cual no ejecutara las demas instrucciones
+        raise TypeError ("no se permiten edades negativas")
     if edad<20:
         print("eres muy joven")
     elif edad<40:
@@ -8,4 +13,4 @@ def evalueEdad(edad):
     elif edad<100:
         print("cuidate... estas anciano")
 
-print (evalueEdad(18))
+print (evalueEdad(-18))#evitar que la edad no puede ser negativa
