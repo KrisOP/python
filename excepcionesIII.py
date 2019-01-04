ece4ece5f6d3d2a658052d8f0c1d5016b573ca6c
KrisOP <kortega070@gmail.com>
2019-01-04 16:35:54 -0600

    nuevo archivo excepcionesIII.py

---
+++ excepcionesIII.py
@@ -0,0 +1,11 @@
+def evalueEdad(edad):
+    if edad<20:
+        print("eres muy joven")
+    elif edad<40:
+        print("joven")
+    elif edad<65:
+        print ("eres maduro")
+    elif edad<100:
+        print("cuidate... estas anciano")
+
+print (evalueEdad(18))
