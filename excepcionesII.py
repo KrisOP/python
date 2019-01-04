97016aa045cddfc77bece6d238d35feebfd471b9
KrisOP <kortega070@gmail.com>
2019-01-04 16:20:00 -0600

    nuevo archivo excepcionesII.py

---
+++ excepcionesII.py
@@ -0,0 +1,8 @@
+def divide():
+    op1=(float(input("introduce el primer numero")))
+    op2=(float(input("introduce el segundo numero")))
+
+    print ("La division es: " + str(op1/op2))
+    print("calculo finalizadoo")
+
+divide
