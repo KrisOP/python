f821d1caa7baedd7a296406b795d306da8eb4b1e
KrisOP <kortega070@gmail.com>
2019-01-04 16:26:53 -0600

    encapsulando varios errores (no recomendado)

--- excepcionesII.py
+++ excepcionesII.py
@@ -4,10 +4,12 @@ def divide():
         op2=(float(input("introduce el segundo numero")))
 
         print ("La division es: " + str(op1/op2))
-    except ValueError:
-        print ("El valor introducido es erroneo")
+    #except ValueError:
+    except:
+        print ("ha ocurrido un error")
+        """print ("El valor introducido es erroneo")
     except ZeroDivisionError:
-        print ("No se puede dividir entre 0")
+        print ("No se puede dividir entre 0")"""
     print("calculo finalizadoo")
 
 divide()
