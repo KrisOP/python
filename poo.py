70458c045f1c93b1ccd52e12ef8a0f5d982ca8f8
KrisOP <kortega070@gmail.com>
2019-01-08 09:01:10 -0600

    uso de metodos en objetos independientes

--- poo.py
+++ poo.py
@@ -5,22 +5,27 @@ class Coche():
     enmarcha=False
     
     #declaracion de metodo no de funcion. (Las funciones no pertenecen a clases, los metodos si)
-    def arrancar(self): #el self viene siendo igual al this en otros lenguajes##recibe como parametro al propio objeto#es decir miCoche.enmarcha=True
-       self.enmarcha=True 
-    def estado(self):
-        if(self.enmarcha):
-            return "el coche esta en marcha"
-        else:
+    def arrancar(self,arrancamos): #el self viene siendo igual al this en otros lenguajes##recibe como parametro al propio objeto#es decir miCoche.enmarcha=True
+       self.enmarcha=arrancamos 
+       if(self.enmarcha):
+          return "el coche esta en marcha"
+       else:
             return "el coche esta detenido"
+
+
+
+    def estado(self):
+
+        print ("el coche tiene ", self.ruedas, "ruedas. Un ancho de ", self.anchoChasis, "y un largo de ", self.largoChasis)
 
 
 miCoche=Coche()#instanciando clase
 
 print ("El largo del coche es: ", miCoche.largoChasis)
 print ("El coche tiene: ",miCoche.ruedas, "ruedas")
-miCoche.arrancar()##llamada al metodo
+print(miCoche.arrancar(True))##llamada al metodo
 
-print (miCoche.estado())
+miCoche.estado()
 
 
 print("***************a continuacion creamos el segundo objeto*********************")
@@ -28,5 +33,5 @@ print("***************a continuacion creamos el segundo objeto******************
 miCoche2=Coche()
 print ("El largo del coche es: ", miCoche.largoChasis)
 print ("El coche tiene: ",miCoche.ruedas, "ruedas")
-
-print (miCoche2.estado())
+print(miCoche2.arrancar(False))
+miCoche2.estado()
