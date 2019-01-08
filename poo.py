ef9ed9ab16158094fa8b24599feff72ba3be75ac
KrisOP <kortega070@gmail.com>
2019-01-08 09:08:49 -0600

    agregando constructor

--- poo.py
+++ poo.py
@@ -1,8 +1,10 @@
 class Coche():
-    largoChasis=250
-    anchoChasis=120
-    ruedas=4
-    enmarcha=False
+
+    def __init__(self):#constructor de la clase (estado inicial de la clase)
+        self.largoChasis=250
+        self.anchoChasis=120
+        self.ruedas=4
+        self.enmarcha=False
     
     #declaracion de metodo no de funcion. (Las funciones no pertenecen a clases, los metodos si)
     def arrancar(self,arrancamos): #el self viene siendo igual al this en otros lenguajes##recibe como parametro al propio objeto#es decir miCoche.enmarcha=True
