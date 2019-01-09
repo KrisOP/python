ebd79c00642b91dfd04f73ed333a5de2e8465670
KrisOP <kortega070@gmail.com>
2019-01-09 16:14:00 -0600

    definiendo clases

---
+++ polimorfismo.py
@@ -0,0 +1,11 @@
+class Coche():
+    def desplazamiento(self):
+        print ("me desplazo usando 4 ruedas")
+
+class Moto():
+    def desplazamiento(self):
+        print ("me desplazo utilizando 2 ruedas")
+
+class Camion():
+    def desplazamiento(self):
+        print ("me desplazo utilizando 6 ruedas")
