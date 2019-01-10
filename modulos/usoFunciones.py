867aefad6880d86ad3354032943b198a9295cec2
KrisOP <kortega070@gmail.com>
2019-01-10 09:22:40 -0600

    importando funciones de modulos

---
+++ modulos/usoFunciones.py
@@ -0,0 +1,10 @@
+#import funcionesMatematicas #importando modulo
+
+#funcionesMatematicas.sumar(7,5)##llamada a funciones del modulo
+
+#IMPORTANDO  MODULO Y FUNCION ESPECIFICA A LA VEZ
+#from funcionesMatematicas import suma
+from funcionesMatematicas import * #importando todas las funciones
+
+sumar(5,7)
+restar(7,3)
