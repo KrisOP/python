97ba9b8efb60a294cd7a3205c84255ae4da37743
KrisOP <kortega070@gmail.com>
2019-01-10 08:59:36 -0600

    usos practicos upper, lower y capitalize

--- metodosCadenas.py
+++ metodosCadenas.py
@@ -1,3 +1,7 @@
 nombreUsuario=input("Introduce tu nombre")
 
-print ("Tu nombre es", nombreUsuario)
+print ("Tu nombre es", nombreUsuario.upper())#convertir la cadena en mayuscula
+print ("Tu nombre es", nombreUsuario.lower())#minuscula
+print ("Tu nombre es", nombreUsuario.capitalize())#primer letra en mayuscula
+
+
