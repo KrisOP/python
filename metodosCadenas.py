56101ca28b6b9aae388a161d2ab7de5572e0eb81
KrisOP <kortega070@gmail.com>
2019-01-10 09:05:55 -0600

    uso de isdigit()

--- metodosCadenas.py
+++ metodosCadenas.py
@@ -1,7 +1,20 @@
-nombreUsuario=input("Introduce tu nombre")
+nombreUsuario=input("Introduce tu nombre ")
 
 print ("Tu nombre es", nombreUsuario.upper())#convertir la cadena en mayuscula
 print ("Tu nombre es", nombreUsuario.lower())#minuscula
 print ("Tu nombre es", nombreUsuario.capitalize())#primer letra en mayuscula
 
+
+edad=input("Introduce la edad ")
+
+#print (edad.isdigit())#vdevuelve true si es un numero
+
+while(edad.isdigit()==False):
+    print("por favor introduce un valor numerico")
+    edad=input("Introduce la edad de nuevo ")
+
+if (int(edad)<18):
+    print ("No puede pasar")
+else:
+    print ("Puede pasar")
 
