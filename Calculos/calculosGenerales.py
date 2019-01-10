982452aba82da64aa99aea41847fc46edff06f94
KrisOP <kortega070@gmail.com>
2019-01-10 11:11:04 -0600

    definiendo funciones

---
+++ Calculos/calculosGenerales.py
@@ -0,0 +1,17 @@
+def sumar(op1,op2):
+    print ("el resultado de la suma es: ",op1+op2)
+
+def restar(op1,op2):
+    print ("el resultado de la resta es: ",op1-op2)
+def multiplicar(op1,op2):
+    print ("el resultado de la multiplicacion es: ",op1*op2)
+
+def dividir(op1,op2):
+    print ("el resultado de la division es: ",op1/op2)
+
+def potencia(op1,op2):
+    print ("el resultado de la potencia es: ",op1**op2)
+
+def redondear(op1):
+    print ("el resultado redondeado es: ",round(op1))
+
