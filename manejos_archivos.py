b6a67dba51c2c33ff91bea2c260a8ddb213b9ad7
KrisOP <kortega070@gmail.com>
2019-01-11 11:14:59 -0600

    uso de funcion append() para agregar mas informacion a un archivo

--- manejos_archivos.py
+++ manejos_archivos.py
@@ -13,16 +13,25 @@ archivo_texto.close()#despues de escribir en el archivo cerrar abierto en memori
 """
 
 #r >> modo escritura
-archivo_texto=open("archivo.txt","r")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
+#archivo_texto=open("archivo.txt","r")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
 
 """texto=archivo_texto.read()#abrir el archivo y leer el contenido y almacenarlo en texto
 
 archivo_texto.close()
 
 print (texto)"""
-
-lineas_texto=archivo_texto.readlines()##guardar la informaccion en una lista
+#######
+"""lineas_texto=archivo_texto.readlines()##guardar la informaccion en una lista
 
 archivo_texto.close()
 
-print(lineas_texto)
+print(lineas_texto)"""
+
+
+##ABRIR UN ARCHIVO PARA AGREGAR INFORMACION
+#a >> abrir el archivo en modo extencion para agregar mas informacion
+archivo_texto=open("archivo.txt","a")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
+
+archivo_texto.write("\n Y maniana tambien xD")
+
+archivo_texto.close()
