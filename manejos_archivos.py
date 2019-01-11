41fe8388d605790739c12dee9bf9373c342b1e16
KrisOP <kortega070@gmail.com>
2019-01-11 13:48:02 -0600

    uso de funcion seek() para situar el cursor en un archivo en un posicion indicada

--- manejos_archivos.py
+++ manejos_archivos.py
@@ -13,13 +13,14 @@ archivo_texto.close()#despues de escribir en el archivo cerrar abierto en memori
 """
 
 #r >> modo escritura
-#archivo_texto=open("archivo.txt","r")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
+archivo_texto=open("archivo.txt","r")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
 
-"""texto=archivo_texto.read()#abrir el archivo y leer el contenido y almacenarlo en texto
-
+#texto=archivo_texto.read()#abrir el archivo y leer el contenido y almacenarlo en texto
+print (archivo_texto.read())
+archivo_texto.seek(0)#desplazar la posicion del puntero a la posicion 0
+print (archivo_texto.read())#si no usamos seek no imprime de nuevo, porque en la primer llamada el cursor termina al final
 archivo_texto.close()
-
-print (texto)"""
+#print (texto)
 #######
 """lineas_texto=archivo_texto.readlines()##guardar la informaccion en una lista
 
@@ -30,8 +31,8 @@ print(lineas_texto)"""
 
 ##ABRIR UN ARCHIVO PARA AGREGAR INFORMACION
 #a >> abrir el archivo en modo extencion para agregar mas informacion
-archivo_texto=open("archivo.txt","a")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
+"""archivo_texto=open("archivo.txt","a")#dos argumentos 1>el nombre del archivos 2>el modo (lectura o escritura o append<para agregar informarcion al archivo que ya existe y que tambien ya tiene informacion>)
 
 archivo_texto.write("\n Y maniana tambien xD")
 
-archivo_texto.close()
+archivo_texto.close()"""
