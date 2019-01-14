09ebc9a7deb17af2445e057f8e11ce3ed6d4fa33
KrisOP <kortega070@gmail.com>
2019-01-14 09:11:44 -0600

    creando clase Persona

--- info_permantente.py
+++ info_permantente.py
@@ -1,2 +1,14 @@
 import pickle
 
+class Persona:
+    def __init__(self, nombre,genero,edad):
+        self.nombre=nombre
+        self.genero=genero
+        self.edad=edad
+        print ("se ha creado una persona nueva con el nombre de ", self.nombre)
+
+    def __str__(self):#convertir en cadena de texto la informacion de un objeto, para verlos en un archivo externo
+        return "{} {} {} ".format(self.nombre,self.genero,self.edad)
+
+
+p=Persona("kris","hombre","22")
