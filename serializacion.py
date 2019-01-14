bca2b9c50959f367e4438acf77f3de13bbcb5247
KrisOP <kortega070@gmail.com>
2019-01-14 08:24:44 -0600

    accediendo al fichero externo en binario y cargando su contenido con picke.load

--- serializacion.py
+++ serializacion.py
@@ -1,6 +1,7 @@
 import pickle
 
-lista_nombres=["Pedro", "Ana", "Maria", "Isabel"]
+#volvar un archivo externo a binario
+"""lista_nombres=["Pedro", "Ana", "Maria", "Isabel"]
 
 fichero_binario=open("lista_nombres","wb")#creadno un archivo externo, con permiso de escritura binaria
 
@@ -10,5 +11,12 @@ fichero_binario.close()
 
 del (fichero_binario) #borrar de la memoria el archivo en ejecucion
 
+"""
 
+#rescatar, leer un archivo externo volcado en binario
 
+fichero=open("lista_nombres","rb")
+
+lista=pickle.load(fichero)
+
+print (lista)
