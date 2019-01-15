ce71adfdaa19468a3786bebc405eb0c672c072c2
KrisOP <kortega070@gmail.com>
2019-01-15 11:19:28 -0600

    aplicando bordes al frame

--- Interfaces/interfacesI.py
+++ Interfaces/interfacesI.py
@@ -12,11 +12,14 @@ raiz.iconbitmap(r'C:\Users\KrisOP\Desktop\Phyton\Interfaces\ff.ico')#agregar ico
 raiz.config(bg="blue")#cambiar el color de fondo de la pantalla
 
 miFrame=Frame() # construir un frame
-#miFrame.pack(fill="x")#redimencionando horizontalmente
+miFrame.pack(fill="x")#redimencionando horizontalmente
 #miFrame.pack(fill="y",expand="true")#redimencionando verticalmente
-miFrame.pack(fill="both",expand="true")#redimencionando en toda la raiz
+#miFrame.pack(fill="both",expand="true")#redimencionando en toda la raiz
 
 #miFrame.pack(side="right", anchor="n")#empaquetar el frame para que este en el interior de la raiz//al redimencionar se ancla en la parte derecha y arriba
 miFrame.config(bg="red")
 miFrame.config(width="650",height="350")
+
+miFrame.config(relief="groove")
+miFrame.config(bd=35)
 raiz.mainloop()#mainloop es equivalent a un ciclo infinito para mantener la interfaz en continua ejecucion
