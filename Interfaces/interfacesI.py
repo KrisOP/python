4eb44823d2888d46e53180d8f8cda544b8a8e520
KrisOP <kortega070@gmail.com>
2019-01-15 11:08:48 -0600

    redimencionando el frame horizontalmente con fill='x'

--- Interfaces/interfacesI.py
+++ Interfaces/interfacesI.py
@@ -12,7 +12,8 @@ raiz.iconbitmap(r'C:\Users\KrisOP\Desktop\Phyton\Interfaces\ff.ico')#agregar ico
 raiz.config(bg="blue")#cambiar el color de fondo de la pantalla
 
 miFrame=Frame() # construir un frame
-miFrame.pack(side="right", anchor="n")#empaquetar el frame para que este en el interior de la raiz//al redimencionar se ancla en la parte derecha y arriba
+miFrame.pack(fill="x")#redimencionando horizontalmente
+#miFrame.pack(side="right", anchor="n")#empaquetar el frame para que este en el interior de la raiz//al redimencionar se ancla en la parte derecha y arriba
 miFrame.config(bg="red")
 miFrame.config(width="650",height="350")
 raiz.mainloop()#mainloop es equivalent a un ciclo infinito para mantener la interfaz en continua ejecucion
