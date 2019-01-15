693a9a7d4e4d3ddbe62fd6f5e0fa99bb1e59eb0a
KrisOP <kortega070@gmail.com>
2019-01-15 11:04:49 -0600

    uso de metodo pack() para anclar la posicion del frame en la raiz

--- Interfaces/interfacesI.py
+++ Interfaces/interfacesI.py
@@ -7,10 +7,12 @@ raiz.title("Ventana de pruebas")
 
 #raiz.iconbitmap('.ico')
 raiz.iconbitmap(r'C:\Users\KrisOP\Desktop\Phyton\Interfaces\ff.ico')#agregar icono a la ventana
-raiz.geometry("650x350")#definir las dimensiones de la ventana
-raiz.resizable(1,0)#evitar redimencionar la ventana//width y height//podemos utilizar true y false tambien
+#raiz.geometry("650x350")#definir las dimensiones de la ventana
+#raiz.resizable(1,0)#evitar redimencionar la ventana//width y height//podemos utilizar true y false tambien
 raiz.config(bg="blue")#cambiar el color de fondo de la pantalla
 
 miFrame=Frame() # construir un frame
-miFrame.pack()#empaquetar el frame para que este en el interior de la raiz
+miFrame.pack(side="right")#empaquetar el frame para que este en el interior de la raiz//al redimencionar se ancla en la parte derecha
+miFrame.config(bg="red")
+miFrame.config(width="650",height="350")
 raiz.mainloop()#mainloop es equivalent a un ciclo infinito para mantener la interfaz en continua ejecucion
