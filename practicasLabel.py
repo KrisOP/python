738e3e29c4da2dfd92a5bdce7e7583c0b466aae9
KrisOP <kortega070@gmail.com>
2019-01-15 13:36:28 -0600

    importando imagenes y ubicandolos

--- practicasLabel.py
+++ practicasLabel.py
@@ -6,7 +6,9 @@ miFrame=Frame(root,width=500, height=400)
 
 miFrame.pack()
 
-miLabel=Label(miFrame,text="Hola Mundo xD",fg="red",font=("Comic Sans MS",18)).place(x=100,y=200)
+miImagen=PhotoImage(file="opcionesLabel.jpg")
+#miLabel=Label(miFrame,text="Hola Mundo xD",fg="red",font=("Comic Sans MS",18)).place(x=100,y=200)
+Label(miFrame,image=miImagen).place(x="100",y="200")
 
 #miLabel.place(x=100,y=200)
 root.mainloop()
