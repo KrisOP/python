0ffe4b6d8ecc07f26578c2075d4658efb412eb9b
KrisOP <kortega070@gmail.com>
2019-01-16 09:40:51 -0600

    definiendo cuadros de textos y label ubicandolos con grid()

--- entry.py
+++ entry.py
@@ -6,11 +6,20 @@ raiz=Tk()
 miFrame=Frame(raiz,width=1200,height=600)
 miFrame.pack()
 
-cuadroTexto=Entry(miFrame)
-cuadroTexto.place(x=100,y=100)
-
 nombreLabel=Label(miFrame,text="Nombre: ")
-nombreLabel.place(x=120,y=100)
+nombreLabel.grid(row=0,column=0)
+cuadroNombre=Entry(miFrame)
+cuadroNombre.grid(row=0,column=1)#dividir el frame en celdas con columnas y filas para ubicar el entry
+
+ApellidoLabel=Label(miFrame,text="Apellido: ")
+ApellidoLabel.grid(row=2,column=0)
+cuadroApellido=Entry(miFrame)
+cuadroApellido.grid(row=2,column=1)
+
+DireccionLabel=Label(miFrame,text="Direccion: ")
+DireccionLabel.grid(row=3,column=0)
+cuadroDireccion=Entry(miFrame)
+cuadroDireccion.grid(row=3,column=1)
 
 
 raiz.mainloop()
