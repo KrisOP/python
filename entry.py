d95fa456866e12e71c9a4144e40c2b7c819efe4d
KrisOP <kortega070@gmail.com>
2019-01-16 09:50:53 -0600

    uso de padx y pady

--- entry.py
+++ entry.py
@@ -7,17 +7,17 @@ miFrame=Frame(raiz,width=1200,height=600)
 miFrame.pack()
 
 nombreLabel=Label(miFrame,text="Nombre: ")
-nombreLabel.grid(row=0,column=0,sticky="e")#sticky es para ubicar el label al este
+nombreLabel.grid(row=0,column=0,sticky="e",padx=10,pady=10)#sticky es para ubicar el label al este
 cuadroNombre=Entry(miFrame)
 cuadroNombre.grid(row=0,column=1)#dividir el frame en celdas con columnas y filas para ubicar el entry
 
 ApellidoLabel=Label(miFrame,text="Apellido: ")
-ApellidoLabel.grid(row=2,column=0,sticky="e")
+ApellidoLabel.grid(row=2,column=0,sticky="e",padx=10,pady=10)
 cuadroApellido=Entry(miFrame)
 cuadroApellido.grid(row=2,column=1)
 
 DireccionLabel=Label(miFrame,text="Direccion: ")
-DireccionLabel.grid(row=3,column=0,sticky="e")
+DireccionLabel.grid(row=3,column=0,sticky="e",padx=10,pady=10)
 cuadroDireccion=Entry(miFrame)
 cuadroDireccion.grid(row=3,column=1)
 
