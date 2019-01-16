df2cf7c8b546ef15345d009203c5caf6aff44264
KrisOP <kortega070@gmail.com>
2019-01-16 09:45:27 -0600

    use sticky

--- entry.py
+++ entry.py
@@ -7,17 +7,17 @@ miFrame=Frame(raiz,width=1200,height=600)
 miFrame.pack()
 
 nombreLabel=Label(miFrame,text="Nombre: ")
-nombreLabel.grid(row=0,column=0)
+nombreLabel.grid(row=0,column=0,sticky="e")#sticky es para ubicar el label al este
 cuadroNombre=Entry(miFrame)
 cuadroNombre.grid(row=0,column=1)#dividir el frame en celdas con columnas y filas para ubicar el entry
 
 ApellidoLabel=Label(miFrame,text="Apellido: ")
-ApellidoLabel.grid(row=2,column=0)
+ApellidoLabel.grid(row=2,column=0,sticky="e")
 cuadroApellido=Entry(miFrame)
 cuadroApellido.grid(row=2,column=1)
 
 DireccionLabel=Label(miFrame,text="Direccion: ")
-DireccionLabel.grid(row=3,column=0)
+DireccionLabel.grid(row=3,column=0,sticky="e")
 cuadroDireccion=Entry(miFrame)
 cuadroDireccion.grid(row=3,column=1)
 
