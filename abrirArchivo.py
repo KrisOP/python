868c8bdbeeb8c1d7b0c30293a6d992101a17a48c
KrisOP <kortega070@gmail.com>
2019-01-23 10:49:00 -0600

    busqueda de archivo, filtrando por extensiones

--- abrirArchivo.py
+++ abrirArchivo.py
@@ -4,8 +4,8 @@ from tkinter import filedialog
 root=Tk()
 
 def abreFichero():
-    fichero=filedialog.askopenfile(title="Abrir")
-    print(fichero)
+    fichero=filedialog.askopenfile(title="Abrir",initialdir="E:",filetypes=(("Ficheros de Excel","*.xlsx"),("Ficheros de Texto","*.txt")))#al abrir la ventana definimos en que directoria se abrira//especificamos el tipo de archivo que queremos mostrar en un menu desplegable 
+    print(fichero)#de momento se imprime la ruta
 
 Button(root,text="Abrir fichero", command=abreFichero).pack()
 
