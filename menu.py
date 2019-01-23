33c6eb02c8f2ef68aee22000e151670319b161f9
KrisOP <kortega070@gmail.com>
2019-01-23 09:56:17 -0600

    agregando opciones del menu

--- menu.py
+++ menu.py
@@ -1,6 +1,18 @@
- from tkinter import *
+from tkinter import *
 
- root=tk()
+root=Tk()
 
+barraMenu=Menu(root)
+root.config(menu=barraMenu,width=300,height=300)
 
- root.mainloop()
+archivoMenu=Menu(barraMenu)#opciones del menu
+archivoEdicion=Menu(barraMenu)
+archivoHerramienta=Menu(barraMenu)
+archivoAyuda=Menu(barraMenu)
+
+barraMenu.add_cascade(label="Archivo",menu=archivoMenu)#la opcion archivoMenu (el elemento) tendra el nombre "archivo"
+barraMenu.add_cascade(label="Edicion",menu=archivoEdicion)
+barraMenu.add_cascade(label="Herramienta",menu=archivoHerramienta)
+barraMenu.add_cascade(label="Ayuda",menu=archivoAyuda)
+
+root.mainloop()
