21ffc88386160cb49b713bb11df30d42bc0bb1c9
KrisOP <kortega070@gmail.com>
2019-01-23 00:08:29 -0600

    funcionalidad a los cheksbottons

--- checksButton.py
+++ checksButton.py
@@ -5,6 +5,24 @@ root=Tk()
 
 root.title("ejemplo")
 
+playa=IntVar()
+montania=IntVar()
+turismoRural=IntVar()
+
+def opcionesViaje():
+    opcionEscogida=""
+
+    if (playa.get()==1):
+        opcionEscogida+="Playa"
+    
+    if (montania.get()==1):
+        opcionEscogida+="Montania"
+
+    if (turismoRural.get()==1):
+        opcionEscogida+="Turismo Rural"
+
+    textoFinal.config(text=opcionEscogida)
+
 
 #foto=PhotoImage(file="patricio.png")#r'C:\Users\KrisOP\Desktop\Phyton\Interfaces\ff.ico'
 
@@ -14,8 +32,11 @@ frame=Frame(root)
 frame.pack()
 
 Label(frame,text="Elije destinos", width=50).pack()
-Checkbutton(root,text="Playa").pack()
-Checkbutton(root,text="Montania").pack()
-Checkbutton(root,text="Turismo Rural").pack()
+Checkbutton(root,text="Playa",variable=playa,onvalue=1,offvalue=0,command=opcionesViaje).pack()#cuando este seleccionado el valor vale 1 y sino vale 0
+Checkbutton(root,text="Montania",variable=montania,onvalue=1,offvalue=0,command=opcionesViaje).pack()
+Checkbutton(root,text="Turismo Rural",variable=turismoRural,onvalue=1,offvalue=0,command=opcionesViaje).pack()
 
+
+textoFinal=Label(frame)
+textoFinal.pack()
 root.mainloop()
