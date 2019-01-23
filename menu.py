f75d723fd36ac36d5c217c045a7cca93cd60c3e4
KrisOP <kortega070@gmail.com>
2019-01-23 10:03:54 -0600

    agregando demas opciones y retocando interfaz grafica

--- menu.py
+++ menu.py
@@ -9,11 +9,20 @@ archivoMenu=Menu(barraMenu,tearoff=0)#opciones del menu
 archivoMenu.add_command(label="Nuevo")#opciones secundarias
 archivoMenu.add_command(label="Guardar")
 archivoMenu.add_command(label="Guardar Como")
+archivoMenu.add_separator()#agregar una linea separadora
+archivoMenu.add_command(label="Cerrar")
 archivoMenu.add_command(label="Salir")
 
-archivoEdicion=Menu(barraMenu)
-archivoHerramienta=Menu(barraMenu)
-archivoAyuda=Menu(barraMenu)
+archivoEdicion=Menu(barraMenu,tearoff=0)
+archivoEdicion.add_command(label="Copiar")
+archivoEdicion.add_command(label="Cortar")
+archivoEdicion.add_command(label="Pegar")
+
+archivoHerramienta=Menu(barraMenu,tearoff=0)
+
+archivoAyuda=Menu(barraMenu,tearoff=0)
+archivoAyuda.add_command(label="Licencia")
+archivoAyuda.add_command(label="Acerca de...")
 
 barraMenu.add_cascade(label="Archivo",menu=archivoMenu)#la opcion archivoMenu (el elemento) tendra el nombre "archivo"
 barraMenu.add_cascade(label="Edicion",menu=archivoEdicion)
