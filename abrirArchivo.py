e49ebed2ad3d521b1bbbfb469bb03b10bbdb49aa
KrisOP <kortega070@gmail.com>
2019-01-23 10:51:05 -0600

    opcion para mostrar todos los ficheros al realizar busqueda

--- abrirArchivo.py
+++ abrirArchivo.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 root=Tk()
 
 def abreFichero():
-    fichero=filedialog.askopenfile(title="Abrir",initialdir="E:",filetypes=(("Ficheros de Excel","*.xlsx"),("Ficheros de Texto","*.txt")))#al abrir la ventana definimos en que directoria se abrira//especificamos el tipo de archivo que queremos mostrar en un menu desplegable 
+    fichero=filedialog.askopenfile(title="Abrir",initialdir="E:",filetypes=(("Ficheros de Excel","*.xlsx"),("Ficheros de Texto","*.txt"),("Todos los ficheros","*.*")))#al abrir la ventana definimos en que directoria se abrira//especificamos el tipo de archivo que queremos mostrar en un menu desplegable, la ultima opcion ppermite mostrar todos los archivos
     print(fichero)#de momento se imprime la ruta
 
 Button(root,text="Abrir fichero", command=abreFichero).pack()
