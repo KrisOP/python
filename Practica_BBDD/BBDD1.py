9574592b4229247f2f915855893a45ac2d64dd83
KrisOP <kortega070@gmail.com>
2019-01-25 16:02:46 -0600

    uso del commit para confirmar cambios realizados en la base de datos

--- Practica_BBDD/BBDD1.py
+++ Practica_BBDD/BBDD1.py
@@ -4,6 +4,10 @@ miConexion=sqlite3.connect("PrimerBase")
 
 miCursor=miConexion.cursor()#crear puntero
 
-miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20))")
+#miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20))")
 
+
+miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON',15,'Deportes')")
+
+miConexion.commit()#confirmar cambios hechos a la base de datos
 miConexion.close()#cerrar la conexion
