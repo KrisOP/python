8d181c2fa51264e75dd4aef91f204a21daca79f1
KrisOP <kortega070@gmail.com>
2019-01-25 15:55:55 -0600

    creando consulta

--- Practica_BBDD/BBDD1.py
+++ Practica_BBDD/BBDD1.py
@@ -2,6 +2,8 @@ import sqlite3
 
 miConexion=sqlite3.connect("PrimerBase")
 
+miCursor=miConexion.cursor()#crear puntero
 
+miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20))")
 
 miConexion.close()#cerrar la conexion
