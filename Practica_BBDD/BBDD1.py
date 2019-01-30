3954acdb9ad736ed8c20949792a8931063530099
KrisOP <kortega070@gmail.com>
2019-01-29 23:49:51 -0600

    insertando varios datos con tuplas y listas

--- Practica_BBDD/BBDD1.py
+++ Practica_BBDD/BBDD1.py
@@ -7,7 +7,16 @@ miCursor=miConexion.cursor()#crear puntero
 #miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER, SECCION VARCHAR(20))")
 
 
-miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON',15,'Deportes')")
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON',15,'Deportes')")
+
+variosProductos=[
+    ("Camiseta",10,"Deportes"),
+    ("Camion",10,"Jugueteria"),
+    ("Jarron",10,"Ceramica")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)",variosProductos)##ejecuta muchos
+
 
 miConexion.commit()#confirmar cambios hechos a la base de datos
 miConexion.close()#cerrar la conexion
