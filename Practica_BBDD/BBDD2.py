d1e7fb0835e343fe1eb2760849c04e4b20798b64
KrisOP <kortega070@gmail.com>
2019-01-30 00:12:40 -0600

    creando tablas e insertando datos en ella

--- Practica_BBDD/BBDD2.py
+++ Practica_BBDD/BBDD2.py
@@ -4,7 +4,21 @@ miConexion=sqlite3.connect("GestionProductos")
 
 miCursor=miConexion.cursor()
 
+miCursor.execute(''' 
+                    CREATE TABLE PRODUCTOS (
+                    CODIGO_ARTICULO VARVHAR (4) PRIMARY KEY,
+                    NOMBRE_ARTICULO VARCHAR (50),
+                    PRECIO INTEGER,
+                    SECCION VARCHAR(20)) 
+''')
 
+productos=[
+    ("AR01","pelota",20,"jugueteria"),
+    ("AR02","pantalon",15,"confeccion"),
+    ("AR03","destornillador",25,"ferreteria")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)",productos)
 
 miConexion.commit()#confirmar cambios hechos en base de datos
 miConexion.close()
